2e635079a3ae96c1f750ae896c7c5a15c3b7f3df
kianoushit1999 <kianoushit1999@gmail.com>
2024-08-09 15:41:45 +0330

    adding complementary styles to the menu and items menu

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,16 +1,27 @@
 import { formatCurrency } from "../../utils/helpers";
+import Button from "../../ui/Button";
 
 function MenuItem({ pizza }) {
   const { name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+    <li className="flex gap-3">
+      <img
+        className={`h-24 ${soldOut && "opacity-75 grayscale"}`}
+        src={imageUrl}
+        alt={name}
+      />
+      <div className="flex grow flex-col">
+        <p className="font-semibold">{name}</p>
+        <p className="text-sm text-stone-500">{ingredients.join(", ")}</p>
+        <div className="mt-auto flex items-center justify-between py-2 text-sm">
+          {!soldOut ? (
+            <p className="">{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="font-medium uppercase text-stone-500">Sold out</p>
+          )}
+
+          <Button type="sm">Add to cart</Button>
         </div>
       </div>
     </li>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,10 +1,20 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const className =
-    "duration-400 inline-block rounded-full bg-yellow-400 px-4 py-2 text-sm font-medium uppercase tracking-wider transition-all hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-1 disabled:cursor-not-allowed md:px-6 md:py-3";
+function Button({ children, disabled, to, type = "primary" }) {
+  const baseClass =
+    "duration-400 inline-block rounded-full bg-yellow-400  font-medium uppercase  transition-all hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-1 disabled:cursor-not-allowed ";
 
-  if (to) return <Link to={to} className={className}>{children}</Link>;
+  const className =
+    type === "sm"
+      ? baseClass + "tracking-tight text-xs px-2 py-1 sm:px-3 sm:py-2"
+      : baseClass + "px-4 py-2 text-sm tracking-wider md:px-6 md:py-3";
+
+  if (to)
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
 
   return (
     <button disabled={disabled} className={className}>
